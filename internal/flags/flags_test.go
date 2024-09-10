66f6a32c21d550fe6e15649c2fc70b61c135362b
Markus Keil <markus@keil-connect.com>
2024-09-10 15:29:56 +0200

    change test to use the env HOME instead of what user returns

--- internal/flags/flags_test.go
+++ internal/flags/flags_test.go
@@ -41,7 +41,7 @@ func TestPathExpansion(t *testing.T) {
 	} else {
 		tests = map[string]string{
 			`/home/someuser/tmp`:        `/home/someuser/tmp`,
-			`~/tmp`:                     user.HomeDir + `/tmp`,
+			`~/tmp`:                     os.Getenv("HOME") + `/tmp`,
 			`~thisOtherUser/b/`:         `~thisOtherUser/b`,
 			`$DDDXXX/a/b`:               `/tmp/a/b`,
 			`/a/b/`:                     `/a/b`,
